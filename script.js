2836d1ee5d76d04dd28ffade658d652435ed4df1
Alfons Alexios Kola <85795425+alexkola94@users.noreply.github.com>
2022-07-07 15:53:58 +0300

    cleaning some code making some changes.

--- script.js
+++ script.js
@@ -2,4 +2,38 @@ function OpenOrClose() {
   document.getElementById("mySidebar").classList.toggle("open");
   document.getElementById("main").classList.toggle("open");
 }
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+const boxes = document.querySelectorAll(".box");
+
+const checkBoxes = () => {
+  const triggersBottom = (window.innerHeight / 5) * 4;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < triggersBottom) box.classList.add("show");
+    else box.classList.remove("show");
+  });
+};
+
+window.addEventListener("scroll", checkBoxes);
+checkBoxes();
